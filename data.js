9f0914990c2640e5b86fc14868ff38b6d9243833
DianaShebeko <157220722+DianaShebeko@users.noreply.github.com>
2026-01-15 05:24:32 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,188 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010387",
+      "name": "R0010387",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.5709646911970427,
+        "pitch": 0.132534584100215,
+        "fov": 1.391813623840451
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0954204642220482,
+          "pitch": 0.5868054874456181,
+          "rotation": 12.566370614359176,
+          "target": "1-r0010388"
+        },
+        {
+          "yaw": 3.1054246335392266,
+          "pitch": 0.07740725068811827,
+          "rotation": 0,
+          "target": "2-r0010389"
+        },
+        {
+          "yaw": 2.5308649404347205,
+          "pitch": 0.6302841111135606,
+          "rotation": 5.497787143782138,
+          "target": "3-r0010390"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.419478447243865,
+          "pitch": 0.45926599601706997,
+          "title": "Об этом объекте",
+          "text": "Это штатив"
+        },
+        {
+          "yaw": -1.639530117609997,
+          "pitch": -0.004083360295306093,
+          "title": "Выключатель",
+          "text": "Здесь должно быть видео"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010388",
+      "name": "R0010388",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.381605535014719,
+          "pitch": 0.4432277228568253,
+          "rotation": 11.780972450961727,
+          "target": "0-r0010387"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010389",
+      "name": "R0010389",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1240121050987284,
+          "pitch": 0.2430926354078835,
+          "rotation": 6.283185307179586,
+          "target": "0-r0010387"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r0010390",
+      "name": "R0010390",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4698251803765103,
+          "pitch": 0.16918347298798508,
+          "rotation": 0,
+          "target": "0-r0010387"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
